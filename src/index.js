96009b5ad0cb1ed8866ad75c0c83dee9683fa4bf
grvrjt <gaurav1993rajput@gmail.com>
2023-11-16 17:09:56 +0530

    plugin and loader added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 function helloWorld(){
     const element= document.createElement('div');
     element.innerHTML = 'Hello Gaurav Webpack';
+    element.classList.add("header");
     return element;
 }
 
